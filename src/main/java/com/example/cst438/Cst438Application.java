e3eab9a834c5e3ca82e9983e2437a615c734dd20
aimenAlt <ymnghost@gmail.com>
2022-05-10 18:10:00 -0700

    Initial spring commit

---
+++ src/main/java/com/example/cst438/Cst438Application.java
@@ -0,0 +1,13 @@
+package com.example.cst438;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst438Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cst438Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cst438/Cst438ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cst438;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cst438ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
